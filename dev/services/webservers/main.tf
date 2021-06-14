e1a826456883f6796cb2d69bb76510ed22ea606e
Y-Fujikawa <fujikawa0@gmail.com>
2021-06-14 11:25:43 +0900

    change minimum instance.

--- dev/services/webservers/main.tf
+++ dev/services/webservers/main.tf
@@ -6,7 +6,7 @@ module "webservers" {
   source           = "../../../modules/services/webservers"
   cluster_name     = "webservers-dev"
   instance_type    = "t2.nano"
-  min_size         = 2
-  max_size         = 5
-  desired_capacity = 2
+  min_size         = 1
+  max_size         = 2
+  desired_capacity = 1
 }
